0b9814f4c0609d4acc0ee2a9341858cc18030048
Manasvi Pant <88909122+ManasviPant09@users.noreply.github.com>
2023-03-30 00:20:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ mongoose
     console.log(err);
   });
 
-// app.use(express.static("client/build"));
-// app.get("*", (req, res) =>{
-//   res.sendFile(path.resolve(__dirname, "client","build","index.html"));
-// });
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials","true");
+  res.send("API is running successfully!");
+});
 
 app.use(cors());
 app.use(express.json());
