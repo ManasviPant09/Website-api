1cd8f028ec17396aac403a68508dd3cbfd17edc6
Manasvi Pant <88909122+ManasviPant09@users.noreply.github.com>
2023-03-30 00:27:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-app.listen(process.env.PORT || 5000, () => {
+const PORT = process.env.PORT;
+app.listen(PORT || 5000, () => {
   console.log("Backend server is running!");
 });
